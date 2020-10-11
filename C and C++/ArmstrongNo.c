3a7e064f48f11b52a39ab3a1c0504bca3d7e08f6
Bhavya Tyagi <44532443+abz1077@users.noreply.github.com>
2020-10-11 16:41:52 +0530

    Update ArmstrongNo.c

--- C and C++/ArmstrongNo.c
+++ C and C++/ArmstrongNo.c
@@ -1,29 +1,28 @@
 //An ARMSTRONG NO. is one whose sum of all digits to the power of no.of digits each= the no. itself.
 //for eg. for a 3 digit no. an armstrong no. is one, whose sum of cubes of each digit= the no. itself
 
-#include <stdio.h>
-#include <math.h>
-int main()
-{
-    int n;
-    printf("\nEnter a no. to check if it's an Armstrong number:");
-    scanf("%d", &n);
-    int temp = n;
-    int sum = 0;
-    while (temp != 0)
-    {
-        int lastDigit = temp % 10;
-        sum = sum + lastDigit * lastDigit * lastDigit;
-        temp /= 10;
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int num, originalnum, remainder, n=0;
+    float result = 0.0;
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+    originalnum = num;
+    
+    for (originalnum = num; originalnum != 0; ++n){
+        originalnum /= 10;
+        
     }
-    if (sum == n)
-    {
-        printf("\n%d is an Armstrong Number.\n\n", n);
+    
+    for (originalnum = num; originalnum != 0; originalnum /= 10) {
+        remainder = originalnum%10;
+        result += pow(remainder, n);
     }
+    
+    if((int)result == num)
+        printf("%d is an Armstrong Number", num);
     else
-        printf("\n%d is not an Armstrong Number.\n\n", n);
-        printf("Made By:- ISHIKA GUPTA \n\n\n\n\n\n  ");
-
-
+        printf("%d is not an Armstrong Number", num);
     return 0;
 }
